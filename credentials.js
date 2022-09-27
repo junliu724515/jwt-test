e6d42639c672b4d899968538b1c4f2508c734484
Jun Liu <jun.liu@jmcloudservices.com>
2022-09-27 12:22:33 +1000

    first commit

---
+++ credentials.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jun Liu on 21/06/2019.
+ */
+module.exports = {
+    salesforce: {
+        consumer_key: '3MVG9Nvmjd9lcjRkaKiz65moygW.e8ap3.piYffeEHHTGP8ZEQhEVdFsbU.SYuEUQ4iTx1lyZHEQ8dTQT2MCT',
+        consumer_secret: '',
+        url: 'https://test.salesforce.com',
+        username: 'jun.liu@epa.poc'
+    },
+    google: {
+        consumer_key: '',
+        consumer_secret: '',
+        callback_url: ''
+    },
+    session: { secret: 'keyboard cat' }
+};
